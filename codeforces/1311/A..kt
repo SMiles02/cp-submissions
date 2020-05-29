83f97da439b559f82606fde46e06005c34d6f9d5
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2020-05-29 16:32:00 +0200

    Add solution for problem `A - Add Odd or Subtract Even` Link: https://codeforces.com/contest/1311/problem/A Tags: greedy, implementation, math, *800 Ref: https://codeforces.com/contest/1311/submission/81894735

---
+++ codeforces/1311/A..kt
@@ -0,0 +1,109 @@
+// kotlin tips b/c I'm bad
+
+/** useful links
+ * https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/-array/index.html
+ * https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/-array-list/index.html
+ * https://kotlinlang.org/docs/reference/ranges.html
+ */
+
+/** valid ways to sort
+ * 1
+ * val a = nextLongs().sorted() // a is mutable list
+ * 2
+ * val a = arrayListOf<Long>()
+ * a.addAll(nextLongs())
+ * a.sort()
+ */
+
+/** declare 2D array
+ * val ori = Array(n, {IntArray(n)})
+ * val ori = arrayOf(
+intArrayOf(8, 9, 1, 13),
+intArrayOf(3, 12, 7, 5),
+intArrayOf(0, 2, 4, 11),
+intArrayOf(6, 10, 15, 14)
+)
+ */
+
+/** printing variables:
+ * println("${l+1} and $r")
+ * evidently print has high constant factor
+ * print stringbuilder instead?
+ */
+
+// ArrayList to Array: toArray
+
+// IntArray with size:
+// val arr = IntArray(1 shl 20, { 1 })
+
+// lower bound: use binary search
+// https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.collections/binary-search.html
+
+// if/switch
+// https://kotlinlang.org/docs/reference/control-flow.html
+
+// swap:
+// https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/also.html
+// a = b.also { b = a }
+
+/** comparing pairs
+ * val ed = ArrayList<Pair<Int,Pair<Int,Int>>>()
+ed.sortBy {it.first}
+ *  class CustomComparator : Comparator<Pair<Int,Pair<Int,Int>>> {
+override fun compare(o1: Pair<Int,Pair<Int,Int>>, o2: Pair<Int,Pair<Int,Int>>): Int {
+return o1.first.compareTo(o2.first)
+}
+}
+val v = ed.sortedWith(CustomComparator())
+ */
+
+// (tree/hash)(map/set)
+// ??
+
+val MX = 1000005
+val MOD = 998244353
+val SZ = 1 shl 19
+val INF = (1e18).toLong()
+
+fun add(a: Int, b: Int) = (a + b) % MOD
+fun sub(a: Int, b: Int) = (a - b + MOD) % MOD
+fun mul(a: Int, b: Int) = ((a.toLong() * b) % MOD).toInt()
+
+private fun readLn() = readLine()!! // string line
+private fun readInt() = readLn().toInt() // single int
+private fun readLong() = readLn().toLong() // single long
+private fun readDouble() = readLn().toDouble() // single double
+private fun readStrings() = readLn().split(" ") // list of strings
+private fun readInts() = readStrings().map { it.toInt() } // list of ints
+private fun readLongs() = readStrings().map { it.toLong() } // list of longs
+private fun readDoubles() = readStrings().map { it.toDouble() } // list of doubles
+
+//var vect  = IntArray(5) declares array
+//var a = readInts() declares arraylist
+
+fun solve() {
+    var (a, b) = readInts()
+    if (a==b)
+        println(0)
+    else if (a>b)
+    {
+        if ((a-b)%2==1)
+            println(2)
+        else
+            println(1)
+    }
+    else
+    {
+        if ((b-a)%2==0)
+            println(2)
+        else
+            println(1)
+    }
+}
+
+fun main(args: Array<String>) {
+    val T = readInt()
+    for (i in 1..T) solve()
+}
+
+//yes I stole Benq's template, Benq orz
