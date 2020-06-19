e7cd6f98504c34284456412eb8624ef00730da73
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2020-06-19 20:12:00 +0200

    Add solution for problem `F - Yet another 2D Walking` Link: https://codeforces.com/contest/1066/problem/F Tags: dp, *2100 Ref: https://codeforces.com/contest/1066/submission/84355155

--- codeforces/1066/F.cpp
+++ codeforces/1066/F.cpp
@@ -1,32 +1,37 @@
 #include <bits/stdc++.h>
 #define ll long long
 #define sz(x) (int)(x).size()
-#define pii pair<int,int>
+#define pii pair<ll,ll>
 using namespace std;
- 
-map<int,bool> done;
-map<int,pii> l;
-map<int,pii> r;
- 
+
+map<ll,bool> done;
+map<ll,pii> l;
+map<ll,pii> r;
+
 ll min(ll a, ll b)
 {
     if (a<b)
         return a;
     return b;
 }
- 
-ll dist(pair<int,int> p, pair<int,int> q)
+
+void pri(pair<int,int> p)
+{
+    cout<<"p: "<<p.first<<" "<<p.second<<"\n";
+}
+
+ll dist(pair<ll,ll> p, pair<ll,ll> q)
 {
     return abs(p.first-q.first)+abs(p.second-q.second);
 }
- 
+
 int main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0);
-    int n,x,y,z;
+    ll n,x,y,z;
     cin>>n;
-    vector<int> v={0};
-    for (int i=0;i<n;++i)
+    vector<ll> v={0};
+    for (ll i=0;i<n;++i)
     {
         cin>>x>>y;
         z=max(x,y);
@@ -39,9 +44,9 @@ int main()
         }
         else
         {
-            if (y>l[z].second||(y==l[z].second&&x<l[z].first))
+            if ((y>l[z].second)||(y==l[z].second&&x<l[z].first))
                 l[z]={x,y};
-            if (x>r[z].first||(x==r[z].first&&y<r[z].second))
+            if ((x>r[z].first)||(x==r[z].first&&y<r[z].second))
                 r[z]={x,y};
         }
     }
@@ -50,10 +55,11 @@ int main()
     sort(v.begin(), v.end());
     dp[0][0]=0;
     dp[0][1]=0;
-    for (int i=1;i<z;++i)
+    for (ll i=1;i<z;++i)
     {
         dp[i][0]=dist(l[v[i]],r[v[i]])+min(dp[i-1][0]+dist(l[v[i-1]],r[v[i]]),dp[i-1][1]+dist(r[v[i-1]],r[v[i]]));
         dp[i][1]=dist(l[v[i]],r[v[i]])+min(dp[i-1][0]+dist(l[v[i-1]],l[v[i]]),dp[i-1][1]+dist(r[v[i-1]],l[v[i]]));
+        //cout<<dp[i][0]<<" "<<dp[i][1]<<"\n";
     }
     if (dp[z-1][0]<dp[z-1][1])
         cout<<dp[z-1][0];
