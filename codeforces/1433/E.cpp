c72bdf664493a46dbdeb9f068f399f6f7c83377a
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2020-10-20 19:42:00 +0200

    Add solution for problem `E - Two Round Dances` Link: https://codeforces.com/contest/1433/problem/E Tags: combinatorics, math, *1300 Ref: https://codeforces.com/contest/1433/submission/96174360

--- codeforces/1433/E.cpp
+++ codeforces/1433/E.cpp
@@ -2,15 +2,29 @@
 #define ll long long
 using namespace std;
 
+ll fact(int x)
+{
+    ll ans=1;
+    for (int i=1;i<=x;++i)
+        ans*=i;
+    return ans;
+}
+
+ll sq(ll a)
+{
+    return a*a;
+}
+
+ll c(ll n, ll k)
+{
+    return fact(n)/(fact(k)*fact(n-k));
+}
+
 int main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0);
-    ll f[21];
-    f[0]=1;
-    for (int i=1;i<21;++i)
-        f[i]=f[i-1]*i;
     int n;
     cin>>n;
-    cout<<(f[n]/(f[n/2]*f[n/2]*2))*(f[n/2-1]*f[n/2-1]);
+    cout<<(c(n,n/2)*sq(fact((n/2)-1)))/2;
     return 0;
 }
