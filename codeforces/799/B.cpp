ab363c4c2a950a0aa8301f09341194073aa2e318
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2021-01-27 15:44:00 +0100

    Add solution for problem `B - T-shirt buying` Link: https://codeforces.com/contest/799/problem/B Tags: data structures, implementation, *1400 Ref: https://codeforces.com/contest/799/submission/105584087

---
+++ codeforces/799/B.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
+using namespace std;
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+//uniform_int_distribution<int>(1000,10000)(rng)
+
+ll binpow(ll a, ll b)
+{
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+ll gcd(ll a,ll b)
+{
+    if (b==0) return a;
+    return gcd(b,a%b);
+}
+
+string to_upper(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+ 
+string to_lower(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int n,m,c;
+    cin>>n;
+    int p[n],a[n],b[n];
+    set<int> s;
+    map<int,priority_queue<int,vector<int>,greater<int>>> pq;
+    for (int i=0;i<n;++i)
+        cin>>p[i];
+    for (int i=0;i<n;++i)
+        cin>>a[i];
+    for (int i=0;i<n;++i)
+        cin>>b[i];
+    for (int i=0;i<n;++i)
+    {
+        pq[a[i]].push(p[i]);
+        pq[b[i]].push(p[i]);
+    }
+    cin>>m;
+    while (m--)
+    {
+        cin>>c;
+        while (!pq[c].empty()&&s.find(pq[c].top())!=s.end())
+            pq[c].pop();
+        if (pq[c].empty())
+            cout<<"-1 ";
+        else
+        {
+            cout<<pq[c].top()<<" ";
+            s.insert(pq[c].top());
+            pq[c].pop();
+        }
+    }
+    return 0;
+}
