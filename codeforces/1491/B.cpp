feb4ae9527ce781ebf590633475a559291bb7ab0
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2021-02-28 14:46:00 +0100

    Add solution for problem `B - Minimal Cost` Link: https://codeforces.com/contest/1491/problem/B Tags: brute force, math, *1200 Ref: https://codeforces.com/contest/1491/submission/108677457

--- codeforces/1491/B.cpp
+++ codeforces/1491/B.cpp
@@ -1,5 +1,40 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+//uniform_int_distribution<int>(1000,10000)(rng)
+
+ll binpow(ll a, ll b)
+{
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+ll gcd(ll a,ll b)
+{
+    if (b==0) return a;
+    return gcd(b,a%b);
+}
+
+string to_upper(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+ 
+string to_lower(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 void solve()
 {
@@ -18,10 +53,12 @@ void solve()
             cout<<"0\n";
             return;
         }
-    if (s.size()==1)
+    if (sz(s)==1)
+    {
         cout<<v+min(u,v)<<"\n";
-    else
-        cout<<min(u,v)<<"\n";
+        return;
+    }
+    cout<<min(u,v)<<"\n";
 }
 
 int main()
