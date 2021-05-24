fe10a6fa110c89821fe701b432cf2a70f952edf3
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2021-05-24 14:02:00 +0200

    Add solution for problem `E - Ciel and Gondolas` Link: https://codeforces.com/contest/321/problem/E Tags: data structures, divide and conquer, dp, *2600 Ref: https://codeforces.com/contest/321/submission/117167587

--- codeforces/321/E.cpp
+++ codeforces/321/E.cpp
@@ -4,15 +4,28 @@ using namespace std;
 const int N = 4001, K = 801, INF = 1e9;
 int C[N][N], dp[K][N];
 
+inline int scan(){
+    char c = getchar();
+    int x = 0;
+    while(c<'0'||c>'9'){
+            c=getchar();
+        }
+    while(c>='0'&&c<='9'){
+            x=(x<<1)+(x<<3)+c-'0';
+            c=getchar();
+        }
+    return x;
+}
+
 void solve(int i, int l, int r, int optL, int optR)
 {
     if (l>r)
         return;
     int m=l+(r-l)/2, best=INF, k=-1;
     for (int j=optL;j<=min(optR,m);++j)
-        if (dp[i-1][j-1]+C[m][j]<best)
+        if (dp[i-1][j-1]+C[j][m]<best)
         {
-            best=dp[i-1][j-1]+C[m][j];
+            best=dp[i-1][j-1]+C[j][m];
             k=j;
         }
     dp[i][m]=best;
@@ -24,25 +37,26 @@ int main()
 {
     ios_base::sync_with_stdio(0); cin.tie(0);
     int n,k;
-    cin>>n>>k;
+    n=scan();
+    k=scan();
     int u[n+1][n+1];
     for (int i=1;i<=n;++i)
     {
         u[i][0]=0;
         for (int j=1;j<=n;++j)
         {
-            cin>>u[i][j];
+            u[i][j]=scan();
             u[i][j]+=u[i][j-1];
         }
     }
-    for (int j=1;j<=n;++j)
+    for (int i=1;i<=n;++i)
     {
-        C[j][j]=0;
-        for (int i=j-1;i>0;--i)
-            C[j][i]=C[j-1][i]+u[j][j]-u[j][i-1];
+        C[i][i]=0;
+        for (int j=i+1;j<=n;++j)
+            C[i][j]=C[i][j-1]+u[j][j]-u[j][i-1];
     }
     for (int i=1;i<=n;++i)
-        dp[1][i]=C[i][1];
+        dp[1][i]=C[1][i];
     for (int i=2;i<=k;++i)
         solve(i,1,n,1,n);
     cout<<dp[k][n];
