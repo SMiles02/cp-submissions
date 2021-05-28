3c943e1d6d4f3abb31fbdb51a94476088a331d89
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2021-05-28 17:41:00 +0200

    Add solution for problem `C2 - Potions (Hard Version)` Link: https://codeforces.com/contest/1526/problem/C2 Tags: data structures, greedy, *1600 Ref: https://codeforces.com/contest/1526/submission/117658910

--- codeforces/1526/C2.cpp
+++ codeforces/1526/C2.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
 
 int main()
@@ -6,7 +8,7 @@ int main()
     ios_base::sync_with_stdio(0); cin.tie(0);
     int n,k;
     cin>>n;
-    long long s=0;
+    ll s=0;
     priority_queue<int,vector<int>,greater<int>> pq;
     pq.push(0);
     while (n--)
@@ -25,6 +27,6 @@ int main()
             pq.push(k);
         }
     }
-    cout<<pq.size()-1;
+    cout<<sz(pq)-1;
     return 0;
 }
