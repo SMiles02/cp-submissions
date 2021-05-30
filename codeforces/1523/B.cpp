7635e8cbeec0ac95636ec5f57857e7450c9672f6
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2021-05-30 16:53:00 +0200

    Add solution for problem `B - Lord of the Values` Link: https://codeforces.com/contest/1523/problem/B Tags: constructive algorithms, *1100 Ref: https://codeforces.com/contest/1523/submission/117880757

--- codeforces/1523/B.cpp
+++ codeforces/1523/B.cpp
@@ -1,5 +1,40 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+//uniform_int_distribution<int>(1000,10000)(rng)
+
+ll binpow(ll a, ll b)
+{
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+ll gcd(ll a,ll b)
+{
+    if (b==0) return a;
+    return gcd(b,a%b);
+}
+
+string to_upper(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+ 
+string to_lower(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 void solve()
 {
@@ -10,7 +45,14 @@ void solve()
         cin>>a[i];
     cout<<n*3<<"\n";
     for (int i=1;i<=n;i+=2)
-        cout<<"1 "<<i<<" "<<i+1<<"\n1 "<<i<<" "<<i+1<<"\n2 "<<i<<" "<<i+1<<"\n1 "<<i<<" "<<i+1<<"\n1 "<<i<<" "<<i+1<<"\n2 "<<i<<" "<<i+1<<"\n";
+    {
+        cout<<"1 "<<i<<" "<<i+1<<"\n";
+        cout<<"1 "<<i<<" "<<i+1<<"\n";
+        cout<<"2 "<<i<<" "<<i+1<<"\n";
+        cout<<"1 "<<i<<" "<<i+1<<"\n";
+        cout<<"1 "<<i<<" "<<i+1<<"\n";
+        cout<<"2 "<<i<<" "<<i+1<<"\n";
+    }
 }
 
 int main()
