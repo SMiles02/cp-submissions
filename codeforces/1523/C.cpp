aecd02122f2c0108a337a85da05016f183c3a3a0
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2021-05-30 17:26:00 +0200

    Add solution for problem `C - Compression and Expansion` Link: https://codeforces.com/contest/1523/problem/C Tags: brute force, data structures, greedy, implementation, trees, *1600 Ref: https://codeforces.com/contest/1523/submission/117895089

--- codeforces/1523/C.cpp
+++ codeforces/1523/C.cpp
@@ -1,12 +1,48 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+//uniform_int_distribution<int>(1000,10000)(rng)
+
+ll binpow(ll a, ll b)
+{
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+ll gcd(ll a,ll b)
+{
+    if (b==0) return a;
+    return gcd(b,a%b);
+}
+
+string to_upper(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+ 
+string to_lower(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 void solve()
 {
     int n,k;
     cin>>n;
-    vector<int> v = {1};
+    vector<int> v;
     cout<<"1\n";
+    v.push_back(1);
     cin>>k;
     for (int i=1;i<n;++i)
     {
@@ -20,7 +56,7 @@ void solve()
             ++v.back();
         }
         cout<<v[0];
-        for (int j=1;j<(int)v.size();++j)
+        for (int j=1;j<sz(v);++j)
             cout<<"."<<v[j];
         cout<<"\n";
     }
