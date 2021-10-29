0fc3a36c61f02bcd08c88185887ce9552aabf86e
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2021-10-29 18:24:00 +0200

    Add solution for problem `D - Red-Blue Matrix` Link: https://codeforces.com/contest/1606/problem/D Tags: brute force, constructive algorithms, implementation, sortings, *2400 Ref: https://codeforces.com/contest/1606/submission/133516353

--- codeforces/1606/D.cpp
+++ codeforces/1606/D.cpp
@@ -2,11 +2,45 @@
 //#pragma GCC target("avx,avx2,sse,sse2")
 #include <bits/stdc++.h>
 #define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+//uniform_int_distribution<int>(1000,10000)(rng)
+
+ll binpow(ll a, ll b)
+{
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+ll gcd(ll a,ll b)
+{
+    if (b==0) return a;
+    return gcd(b,a%b);
+}
+
+string to_upper(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+ 
+string to_lower(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 const int N = 5e5+7, MOD = 1e9+7;
 int b1[N], b2[N];
+set<ll> s[N];
 
 int add(int a, int b)
 {
@@ -16,14 +50,18 @@ int add(int a, int b)
     return a;
 }
 
+int mul(int a, int b)
+{
+    return (1LL*a*b)%MOD;
+}
+
 void solve()
 {
     int n,m,c1,c2,k;
     cin>>n>>m;
     vector<vector<int>>  a(n,vector<int>(m));
-    vector<int> mn(n,1e6), mx(n), sf(n);
+    vector<int> mn(n,1e6), mx(n), pr(n), sf(n);
     vector<array<int,2>> v(n);
-    vector<set<ll>> s(m);
     for (int i=0;i<m;++i)
         s[i].clear();
     for (int i=0;i<n;++i)
@@ -90,12 +128,14 @@ int main()
     ios_base::sync_with_stdio(0); cin.tie(0);
     b1[0]=1;
     b1[1]=uniform_int_distribution<int>(1000,10000)(rng);
+    // b1[1]=uniform_int_distribution<int>(2,2)(rng);
     for (int i=2;i<N;++i)
-        b1[i]=(1LL*b1[i-1]*b1[1])%MOD;
+        b1[i]=mul(b1[i-1],b1[1]);
     b2[0]=1;
     b2[1]=uniform_int_distribution<int>(1000,10000)(rng);
+    // b2[1]=uniform_int_distribution<int>(2,2)(rng);
     for (int i=2;i<N;++i)
-        b2[i]=(1LL*b2[i-1]*b2[1])%MOD;
+        b2[i]=mul(b2[i-1],b2[1]);
     int t;
     cin>>t;
     while (t--)
