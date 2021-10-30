a2458dbb1ff526e64b5e2d0a2ae5f96e20da4a68
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2021-10-30 16:40:00 +0200

    Add solution for problem `A - Di-visible Confusion` Link: https://codeforces.com/contest/1603/problem/A Tags: constructive algorithms, math, number theory, *1300 Ref: https://codeforces.com/contest/1603/submission/133619545

--- codeforces/1603/A.cpp
+++ codeforces/1603/A.cpp
@@ -1,6 +1,24 @@
+//#pragma GCC optimize("Ofast,unroll-loops")
+//#pragma GCC target("avx,avx2,sse,sse2")
 #include <bits/stdc++.h>
 #define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+//uniform_int_distribution<int>(1000,10000)(rng)
+
+ll binpow(ll a, ll b)
+{
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
 
 ll gcd(ll a,ll b)
 {
@@ -8,24 +26,37 @@ ll gcd(ll a,ll b)
     return gcd(b,a%b);
 }
 
+string to_upper(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+ 
+string to_lower(string a)
+{
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
+
 void solve()
 {
     int n;
     cin>>n;
-    bool ok=0;
-    ll l=1;
+    bool ok = 1;
+    ll l = 1;
     int a[n+1];
     for (int i=1;i<=n;++i)
     {
         cin>>a[i];
         if (l<=1e9)
             l=(l*(i+1))/gcd(l,i+1);
-        ok|=(a[i]%l==0);
+        if (a[i]%l==0)
+            ok=0;
     }
     if (ok)
-        cout<<"NO\n";
-    else
         cout<<"YES\n";
+    else
+        cout<<"NO\n";
 }
   
 int main()
