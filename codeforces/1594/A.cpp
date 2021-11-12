ab601220b2902b101a1209abfd33b80f2b5e53d3
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2021-11-12 15:28:00 +0100

    Add solution for problem `A - Consecutive Sum Riddle` Link: https://codeforces.com/contest/1594/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1594/submission/135102367

---
+++ codeforces/1594/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    long long n;
+    cin>>n;
+    cout<<-n+1<<" "<<n<<"\n";
+}
+  
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int n;
+    cin>>n;
+    while (n--)
+        solve();
+    return 0;
+}
