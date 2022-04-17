fa97ffe1b91937df2955d7ff778504cf51241c19
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2022-04-17 16:42:00 +0200

    Add solution for problem `A - Red Versus Blue` Link: https://codeforces.com/contest/1659/problem/A Tags: constructive algorithms, greedy, implementation, math, *1000 Ref: https://codeforces.com/contest/1659/submission/153892433

---
+++ codeforces/1659/A.cpp
@@ -0,0 +1,62 @@
+// #pragma GCC optimize("Ofast,unroll-loops")
+// #pragma GCC target("avx,avx2,sse,sse2")
+#include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
+using namespace std;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(1000,10000)(rng)
+
+ll binpow(ll a, ll b) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1) res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+ll gcd(ll a,ll b) {
+    if (b==0) return a;
+    return gcd(b,a%b);
+}
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+ 
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
+
+void solve() {
+    int n, r, b;
+    cin >> n >> r >> b;
+    vector<int> v(b * 2 + 1, 1);
+    r -= (b + 1);
+    while (r > 0)
+        for (int i = 0; i < b * 2 + 1 && r > 0; i += 2) {
+            --r;
+            ++v[i];
+        }
+    for (int i = 0; i < b * 2 + 1; ++i)
+        while (v[i]-- > 0) {
+            if (i & 1)
+                cout << "B";
+            else
+                cout << "R";
+        }
+    cout << "\n";
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
