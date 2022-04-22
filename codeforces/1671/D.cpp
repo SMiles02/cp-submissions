5f3b344cb52af7cbcd9b814b1a2a34bccaf04276
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2022-04-22 17:16:00 +0200

    Add solution for problem `D - Insert a Progression` Link: https://codeforces.com/contest/1671/problem/D Tags: brute force, constructive algorithms, greedy, *1600 Ref: https://codeforces.com/contest/1671/submission/154541462

--- codeforces/1671/D.cpp
+++ codeforces/1671/D.cpp
@@ -1,11 +1,42 @@
+// #pragma GCC optimize("Ofast,unroll-loops")
+// #pragma GCC target("avx,avx2,sse,sse2")
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(1000,10000)(rng)
+
+ll binpow(ll a, ll b) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1) res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+ll gcd(ll a,ll b) {
+    if (b==0) return a;
+    return gcd(b,a%b);
+}
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+ 
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 void solve() {
     int n, x;
     cin >> n >> x;
     int a[n], mn = 2e5, mx = 1;
-    long long ans = 0;
+    ll ans = 0;
     for (int i = 0; i < n; ++i) {
         cin >> a[i];
         if (i > 0)
@@ -13,8 +44,11 @@ void solve() {
         mn = min(mn, a[i]);
         mx = max(mx, a[i]);
     }
-    if (x < mn)
+    // cout << ans << "\n";
+    if (x < mn) {
+        // cout << "oop\n";
         ans += min((mn - 1) * 2, min(a[0], a[n - 1]) - 1);
+    }
     else {
         if (1 < mn)
             ans += min((mn - 1) * 2, min(a[0], a[n - 1]) - 1);
