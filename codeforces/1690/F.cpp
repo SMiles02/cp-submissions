9a39ad4839d4b56c8d263e3adc378a0c72db6b23
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2022-06-07 17:19:00 +0200

    Add solution for problem `F - Shifting String` Link: https://codeforces.com/contest/1690/problem/F Tags: graphs, math, number theory, strings, *1700 Ref: https://codeforces.com/contest/1690/submission/159785712

--- codeforces/1690/F.cpp
+++ codeforces/1690/F.cpp
@@ -1,6 +1,20 @@
 #include <bits/stdc++.h>
 #define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+//uniform_int_distribution<int>(1000,10000)(rng)
+
+ll binpow(ll a, ll b) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
 
 ll gcd(ll a, ll b) {
     if (b == 0) return a;
@@ -9,6 +23,16 @@ ll gcd(ll a, ll b) {
 
 ll lcm(ll x, ll y) {
     return (x / gcd(x, y)) * y;
+}
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
 }
 
 struct DSU {
@@ -59,10 +83,14 @@ void solve() {
             }
             m = v.size();
             u = r = t;
+            // for (int i : v)
+            //     cout << i << " ";
+            // cout << "\n" << t << "\n";
             for (int j = 1; j <= m; ++j) {
                 for (int i = 0; i + 1 < m; ++i)
                     r[i] = u[i + 1];
                 r[m - 1] = u[0];
+                // cout << j << ": " << r << "\n";
                 if (r == t) {
                     ans = lcm(ans, j);
                     break;
