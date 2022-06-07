fdb7c78a9b198313e7356c26fcf589eb7f24f44e
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2022-06-07 18:14:00 +0200

    Add solution for problem `G - Count the Trains` Link: https://codeforces.com/contest/1690/problem/G Tags: binary search, data structures, greedy, sortings, *2000 Ref: https://codeforces.com/contest/1690/submission/159834302

--- codeforces/1690/G.cpp
+++ codeforces/1690/G.cpp
@@ -2,6 +2,34 @@
 #define ll long long
 #define sz(x) (int)(x).size()
 using namespace std;
+//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+//uniform_int_distribution<int>(1000,10000)(rng)
+
+ll binpow(ll a, ll b) {
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+ll gcd(ll a, ll b) {
+    if (b == 0) return a;
+    return gcd(b, a % b);
+}
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 namespace internal {
 
@@ -34,6 +62,135 @@ int bsf(unsigned int n) {
 }
 
 }  // namespace internal
+
+template <class S, S (*op)(S, S), S (*e)()> struct segtree {
+  public:
+    segtree() : segtree(0) {}
+    explicit segtree(int n) : segtree(std::vector<S>(n, e())) {}
+    explicit segtree(const std::vector<S>& v) : _n(int(v.size())) {
+        log = internal::ceil_pow2(_n);
+        size = 1 << log;
+        d = std::vector<S>(2 * size, e());
+        for (int i = 0; i < _n; i++) d[size + i] = v[i];
+        for (int i = size - 1; i >= 1; i--) {
+            update(i);
+        }
+    }
+
+    void set(int p, S x) {
+        assert(0 <= p && p < _n);
+        p += size;
+        d[p] = x;
+        for (int i = 1; i <= log; i++) update(p >> i);
+    }
+
+    S get(int p) const {
+        assert(0 <= p && p < _n);
+        return d[p + size];
+    }
+
+    S prod(int l, int r) const {
+        assert(0 <= l && l <= r && r <= _n);
+        S sml = e(), smr = e();
+        l += size;
+        r += size;
+
+        while (l < r) {
+            if (l & 1) sml = op(sml, d[l++]);
+            if (r & 1) smr = op(d[--r], smr);
+            l >>= 1;
+            r >>= 1;
+        }
+        return op(sml, smr);
+    }
+
+    S all_prod() const { return d[1]; }
+
+    template <bool (*f)(S)> int max_right(int l) const {
+        return max_right(l, [](S x) { return f(x); });
+    }
+    template <class F> int max_right(int l, F f) const {
+        assert(0 <= l && l <= _n);
+        assert(f(e()));
+        if (l == _n) return _n;
+        l += size;
+        S sm = e();
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!f(op(sm, d[l]))) {
+                while (l < size) {
+                    l = (2 * l);
+                    if (f(op(sm, d[l]))) {
+                        sm = op(sm, d[l]);
+                        l++;
+                    }
+                }
+                return l - size;
+            }
+            sm = op(sm, d[l]);
+            l++;
+        } while ((l & -l) != l);
+        return _n;
+    }
+
+    template <bool (*f)(S)> int min_left(int r) const {
+        return min_left(r, [](S x) { return f(x); });
+    }
+    template <class F> int min_left(int r, F f) const {
+        assert(0 <= r && r <= _n);
+        assert(f(e()));
+        if (r == 0) return 0;
+        r += size;
+        S sm = e();
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!f(op(d[r], sm))) {
+                while (r < size) {
+                    r = (2 * r + 1);
+                    if (f(op(d[r], sm))) {
+                        sm = op(d[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - size;
+            }
+            sm = op(d[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+
+  private:
+    int _n, size, log;
+    std::vector<S> d;
+
+    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+};
+
+const int INF = 1e9 + 7;
+
+// basic operation
+int op(int a, int b) { return min(a, b); }
+
+// op(anything, e()) = anything
+int e() { return INF; }
+
+// currently query max
+// remember: range l, r means [l, r)
+
+// Constructor:
+// (1) segtree<S, op, e> seg(int n)
+// (2) segtree<S, op, e> seg(vector<S> v)
+
+// Functions:
+// seg.set(int p, S x) => a[p] := x
+// seg.get(int p) => a[p]
+// seg.prod(int l, int r) => op(a[l], ..., a[r - 1])
+// seg.all_prod() => op(a[0], ..., a[n - 1])
+// seg.min_left<cnd>(int r) => minimum l such that g(op(a[l], ..., a[r - 1])) = true
+// => Constraints: g(e()) = true
+// seg.max_right<cnd>(int r) => maximum r such that g(op(a[l], ..., a[r - 1])) = true
+// => Constraints: g(e()) = true
 
 template <class S,
           S (*op)(S, S),
@@ -208,23 +365,21 @@ struct lazy_segtree {
         lz[k] = id();
     }
 };
-
-const int INF = 1e9 + 7;
  
 // values
-using S = array<int, 2>;
+using S = int;
 
 // lazy values
 using F = bool;
 
 // basic operation 
-S l_op(S l, S r) { return {min(l[0], r[0]), l[1] + r[1]}; }
+S l_op(S l, S r) { return l + r; }
 
 // op(anything, e()) = anything
-S l_e() { return {INF, 0}; }
+S l_e() { return 0; }
 
 // how a lazy update affects a value
-S l_mapping(F l, S r) { return {r[0], r[1] * l}; }
+S l_mapping(F l, S r) { return r * l; }
  
 // how lazy update l affects existing lazy r
 F l_composition(F l, F r) { return l & r; }
@@ -232,35 +387,59 @@ F l_composition(F l, F r) { return l & r; }
 // base lazy
 F l_id() { return true; }
 
+// currently update multiply / add, query sum
+// remember: range l, r means [l, r)
+
+// Constructor:
+// (1) lazy_segtree<S, op, e, F, mapping, composition, id> seg(int n);
+// (2) lazy_segtree<S, op, e, F, mapping, composition, id> seg(vector<T> v);
+
+// Functions:
+// seg.set(int p, S x) => a[p] := x
+// seg.get(int p) => a[p]
+// seg.prod(int l, int r) => op(a[l], ..., a[r - 1])
+// seg.all_prod() => op(a[0], ..., a[n - 1])
+// seg.apply(int p, F f) => a[p] = f(a[p])
+// seg.apply(int l, int r, F f) => a[i] = f(a[i]) for all i = l ... r - 1
+// seg.min_left<cnd>(int r) => minimum l such that g(op(a[l], ..., a[r - 1])) = true
+// => Constraints: g(e()) = true
+// seg.max_right<cnd>(int r) => maximum r such that g(op(a[l], ..., a[r - 1])) = true
+// => Constraints: g(e()) = true
+
 int d;
 
-bool f(S x) {
-    return x[0] >= d;
+bool f(int x) {
+    return x >= d;
 }
 
 void solve() {
     int n, m, k, x;
     cin >> n >> m;
     int a[n + 1];
-    lazy_segtree<S, l_op, l_e, F, l_mapping, l_composition, l_id> seg(n + 1);
+    segtree<int, op, e> seg(n + 1);
+    lazy_segtree<S, l_op, l_e, F, l_mapping, l_composition, l_id> l_seg(n + 1);
     for (int i = 1; i <= n; ++i) {
         cin >> a[i];
-        if (a[i] < seg.prod(1, i)[0])
-            seg.set(i, {a[i], 1});
-        else
-            seg.set(i, {a[i], 0});
+        seg.set(i, a[i]);
+        if (a[i] < seg.prod(1, i))
+            l_seg.set(i, 1);
     }
     while (m--) {
         cin >> k >> d;
         a[k] -= d;
         d = a[k];
-        if (d < seg.prod(1, k)[0]) {
-            seg.set(k, {d, 1});
-            seg.apply(k + 1, seg.max_right(k, f), false);
+        seg.set(k, d);
+        // for (int i = 1; i <= n; ++i)
+        //     cerr << a[i] << " ";
+        // cerr << "\n";
+        if (d < seg.prod(1, k)) {
+            // cerr << "fuck\n";
+            l_seg.set(k, 1);
+            l_seg.apply(k + 1, seg.max_right(k, f), false);
         }
         else
-            seg.set(k, {d, 0});
-        cout << seg.all_prod()[1] << " ";
+            l_seg.set(k, 0);
+        cout << l_seg.all_prod() << " ";
     }
     cout << "\n";
 }
