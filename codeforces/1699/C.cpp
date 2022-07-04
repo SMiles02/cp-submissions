db13717c401357c31fbc9d9a3509095077433104
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2022-07-04 17:09:00 +0200

    Add solution for problem `C - The Third Problem` Link: https://codeforces.com/contest/1699/problem/C Tags: combinatorics, constructive algorithms, math, *1700 Ref: https://codeforces.com/contest/1699/submission/162779846

--- codeforces/1699/C.cpp
+++ codeforces/1699/C.cpp
@@ -1,9 +1,39 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
 
-const int MOD = 1e9 + 7;
+const int MOD = 1e9+7, N = 2e5+1;
+int f[N];
 
-int mul(int a, int b) { return (1LL * a * b) % MOD; }
+int add(int a, int b) {
+    a+=b;
+    if (a>=MOD) a-=MOD;
+    return a;
+}
+
+int mul(int a, int b) { return (1LL*a*b)%MOD; }
+
+int binpow(int a, int b) {
+    int res = 1;
+    while (b > 0) {
+        if (b & 1) res = mul(res,a);
+        a = mul(a,a);
+        b >>= 1;
+    }
+    return res;
+}
+
+int dv(int a, int b) { return mul(a,binpow(b,MOD-2)); }
+
+int sub(int a, int b) { return add(a,MOD-b); }
+
+int C(int n, int k) { return dv(f[n],mul(f[k],f[n-k])); }
+
+void calc_factorials() {
+    f[0]=1;
+    for (int i=1;i<N;++i) f[i]=mul(f[i-1],i);
+}
 
 void solve() {
     int n, l, r, ans = 1;
@@ -17,8 +47,10 @@ void solve() {
     for (int i = 1; i < n; ++i) {
         l = min(l, p[i]);
         r = max(r, p[i]);
-        if (p[i] != l && p[i] != r)
+        if (p[i] != l && p[i] != r) {
+            // cerr << i << ": " << r - l - i + 1 << "\n";
             ans = mul(ans, r - l - i + 1);
+        }
     }
     cout << ans << "\n";
 }
