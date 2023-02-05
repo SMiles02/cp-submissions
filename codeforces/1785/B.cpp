dd69a04a4c07a21d06082d3df014314d1d7d41b5
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-02-05 14:25:00 +0100

    Add solution for problem `B - Letter Exchange` Link: https://codeforces.com/contest/1785/problem/B Tags: constructive algorithms, graphs, greedy, implementation, *1900 Ref: https://codeforces.com/contest/1785/submission/192320923

--- codeforces/1785/B.cpp
+++ codeforces/1785/B.cpp
@@ -1,14 +1,19 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
 
-const string S = "win";
 int cur[3];
 vector<int> cnt[3][3];
 
 void solve() {
-    int n;
+    for (int i = 0; i < 4; ++i)
+        for (int j = 0; j < 4; ++j)
+            cnt[i][j].clear();
+    int n, a, b;
     cin >> n;
     string s;
+    string S = "win";
     for (int i = 1; i <= n; ++i) {
         cur[0] = cur[1] = cur[2] = 0;
         cin >> s;
@@ -20,12 +25,14 @@ void solve() {
             else
                 ++cur[2];
         }
+        // cout << cur[0] << " " << cur[1] << " " << cur[2] << endl;
         for (int x = 0; x < 3; ++x)
             for (int y = 0; y < 3; ++y)
                 if (cur[x] > 1 && cur[y] < 1) {
                     --cur[x];
                     ++cur[y];
                     cnt[x][y].push_back(i);
+                    // cout << x << " " << y << endl;
                 }
     }
     vector<array<int, 4>> ans;
@@ -40,10 +47,16 @@ void solve() {
         for (int y = 0; y < 3; ++y)
             while (!cnt[x][y].empty() && !cnt[y][3 - x - y].empty()) {
                 ans.push_back({cnt[x][y].back(), x, cnt[y][3 - x - y].back(), y});
-                ans.push_back({cnt[y][3 - x - y].back(), x, cnt[3 - x - y][x].back(), 3 - x - y});
                 cnt[x][y].pop_back();
+                cnt[x][3 - x - y].push_back(cnt[y][3 - x - y].back());
                 cnt[y][3 - x - y].pop_back();
-                cnt[3 - x - y][x].pop_back();
+                for (int i = 0; i < 3; ++i)
+                    for (int j = 0; j < 3; ++j)
+                        while (!cnt[i][j].empty() && !cnt[j][i].empty()) {
+                            ans.push_back({cnt[i][j].back(), i, cnt[j][i].back(), j});
+                            cnt[i][j].pop_back();
+                            cnt[j][i].pop_back();
+                        }
             }
     cout << ans.size() << "\n";
     for (auto i : ans)
