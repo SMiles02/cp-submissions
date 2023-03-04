7daf50b7c371ea00ebcbcdc06449a230dee7fb6e
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-03-04 18:46:00 +0100

    Add solution for problem `A - Prefix and Suffix Array` Link: https://codeforces.com/contest/1794/problem/A Tags: strings, *800 Ref: https://codeforces.com/contest/1794/submission/196007294

--- codeforces/1794/A.cpp
+++ codeforces/1794/A.cpp
@@ -1,21 +1,60 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+
+bool is_ok(string s, map<string, int> m) {
+    for (int i = 0; i < s.size(); ++i)
+        if (s[i] != s[s.size() - i - 1])
+            return false;
+    string t = "";
+    for (int i = 0; i + 1 < s.size(); ++i) {
+        t += s[i];
+        if (--m[t] < 0)
+            return false;
+    }
+    for (int i = 1; i < s.size(); ++i) {
+        string u = "";
+        for (int j = i; j < s.size(); ++j)
+            u += s[j];
+        if (--m[u] < 0)
+            return false;
+    }
+    return true;
+}
 
 void solve() {
     int n;
     cin >> n;
-    vector<string> v;
+    map<string, int> m;
+    vector<string> v, w;
     for (int i = 0; i < n * 2 - 2; ++i) {
         string s;
         cin >> s;
-        if (s.size() == n - 1)
+        ++m[s];
+        if (s.size() == 1)
             v.push_back(s);
+        if (s.size() == n - 1)
+            w.push_back(s);
     }
-    reverse(v[0].begin(), v[0].end());
-    if (v[0] == v[1])
-        cout << "YES\n";
-    else
-        cout << "NO\n";
+    if (n == 2) {
+        if (v[0] == v[1])
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+        return;
+    }
+    for (auto i : v)
+        for (auto j : w) {
+            string tmp = i;
+            tmp += j;
+            // cout << tmp << endl;
+            if (is_ok(tmp, m)) {
+                cout << "YES\n";
+                return;
+            }
+        }
+    cout << "NO\n";
 }
 
 int main() {
