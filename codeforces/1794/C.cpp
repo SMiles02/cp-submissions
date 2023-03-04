a273f8b27bdd698582d7b2cbf7dfec7620e41c17
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-03-04 18:53:00 +0100

    Add solution for problem `C - Scoring Subsequences` Link: https://codeforces.com/contest/1794/problem/C Tags: binary search, greedy, math, two pointers, *1300 Ref: https://codeforces.com/contest/1794/submission/196011758

--- codeforces/1794/C.cpp
+++ codeforces/1794/C.cpp
@@ -1,15 +1,35 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(1000,10000)(rng)
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 void solve() {
-    int n, l = 0;
+    int n;
     cin >> n;
     int a[n];
-    for (int r = 0; r < n; ++r) {
-        cin >> a[r];
-        while (a[l] < r - l + 1)
-            ++l;
-        cout << r - l + 1 << " ";
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+        int l = 0, r = i, m;
+        while (l < r) {
+            m = l + (r - l) / 2;
+            if (a[m] >= i - m + 1)
+                r = m;
+            else
+                l = m + 1;
+        }
+        cout << i - l + 1 << " ";
     }
     cout << "\n";
 }
