0e15bea7b06706e72b7a2f1875da3b25a6cad283
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-03-12 15:53:00 +0100

    Add solution for problem `C - Pull Your Luck` Link: https://codeforces.com/contest/1804/problem/C Tags: brute force, greedy, math, number theory, *1500 Ref: https://codeforces.com/contest/1804/submission/197083476

--- codeforces/1804/C.cpp
+++ codeforces/1804/C.cpp
@@ -1,16 +1,32 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(1000,10000)(rng)
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
+
+ll f(int x) {
+    return (1LL * x * (x + 1)) / 2;
+}
 
 void solve() {
     int n, x, p;
     cin >> n >> x >> p;
-    for (int i = 1; i <= min(n * 2, p); ++i) {
-        x = (x + i) % n;
-        if (x == 0) {
+    for (int i = 1; i <= min(n * 10, p); ++i)
+        if ((f(i) + x) % n == 0) {
             cout << "Yes\n";
             return;
         }
-    }
     cout << "No\n";
 }
 
