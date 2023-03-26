66ecc6afb714cbf16858b953ccb8045abac03e96
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-03-26 17:28:00 +0200

    Add solution for problem `C - Candy Store` Link: https://codeforces.com/contest/1798/problem/C Tags: greedy, math, number theory, *1700 Ref: https://codeforces.com/contest/1798/submission/199278426

--- codeforces/1798/C.cpp
+++ codeforces/1798/C.cpp
@@ -1,6 +1,19 @@
 #include <bits/stdc++.h>
 #define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(1000,10000)(rng)
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 ll lcm(ll a, ll b) {
     return (a / __gcd(a, b)) * b;
@@ -9,26 +22,35 @@ ll lcm(ll a, ll b) {
 void solve() {
     int n, ans = 0;
     cin >> n;
-    int a[n], b[n], l = 0;
+    int a[n], b[n];
     for (int i = 0; i < n; ++i)
         cin >> a[i] >> b[i];
     ll cur = 1, tmp;
-    for (int r = 0; r < n; ++r) {
-        tmp = lcm(cur, b[r]);
-        bool ok = true;
-        if ((1LL * a[r] * b[r]) % tmp)
-            ok = false;
-        if (cur != tmp)
-            for (int k = l; k < r; ++k)
-                if ((1LL * a[k] * b[k]) % tmp)
-                        ok = false;
-        if (ok)
-            cur = tmp;
-        else {
+    deque<ll> d;
+    for (int i = 0; i < n; ++i) {
+        tmp = lcm(cur, b[i]);
+        if ((1LL * a[i] * b[i]) % tmp) {
             ++ans;
-            l = r;
-            cur = b[r];
+            while (!d.empty())
+                d.pop_back();
+            cur = b[i];
         }
+        else {
+            bool ok = true;
+            if (cur != tmp)
+                for (auto j : d)
+                    if (j % tmp)
+                        ok = false;
+            if (ok)
+                cur = tmp;
+            else {
+                ++ans;
+                while (!d.empty())
+                    d.pop_back();
+                cur = b[i];
+            }
+        }
+        d.push_back(1LL * a[i] * b[i]);
     }
     cout << ans + 1 << "\n";
 }
