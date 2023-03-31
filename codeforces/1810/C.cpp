23258ad5378555955dfd36e461b0f3aece68a377
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-03-31 17:04:00 +0200

    Add solution for problem `C - Make It Permutation` Link: https://codeforces.com/contest/1810/problem/C Tags: brute force, greedy, sortings, *1300 Ref: https://codeforces.com/contest/1810/submission/199980381

--- codeforces/1810/C.cpp
+++ codeforces/1810/C.cpp
@@ -1,5 +1,20 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
+#define int ll
 using namespace std;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(1000,10000)(rng)
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 void solve() {
     int n, cnt = 0, c, d, unq = 0;
@@ -13,12 +28,13 @@ void solve() {
     for (int i = 0; i < n; ++i)
         a[i + n] = i + 1;
     sort(a, a + n * 2);
-    long long ans = 1e18, cur = 0;
+    ll ans = 1e18, cur = 0;
     for (int i = 0; i < n * 2; ++i) {
-        unq += min(m[a[i]], 1);
+        unq += min(m[a[i]], 1LL);
         cnt += m[a[i]];
-        cur += 1LL * max(m[a[i]] - 1, 0) * c;
-        ans = min(ans, cur + 1LL * (n - cnt) * c + 1LL * (a[i] - unq) * d);
+        cur += 1LL * max(m[a[i]] - 1, 0LL) * c;
+        ans = min(ans, cur + (n - cnt) * c + (a[i] - unq) * d);
+        // cerr << cur + (n - cnt) * c + (a[i] - unq) * d << "\n";
         m[a[i]] = 0;
     }
     cout << ans << "\n";
