d5409d2e6d70d5047fd68f35398f7a06bc796859
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-03-31 16:47:00 +0200

    Add solution for problem `B - Candies` Link: https://codeforces.com/contest/1810/problem/B Tags: constructive algorithms, math, number theory, *800 Ref: https://codeforces.com/contest/1810/submission/199968923

--- codeforces/1810/B.cpp
+++ codeforces/1810/B.cpp
@@ -1,26 +1,49 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(1000,10000)(rng)
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 void solve() {
-    int n;
+    int n, cur = 3;
     cin >> n;
     if (n % 2 == 0) {
         cout << "-1\n";
         return;
     }
-    vector<int> ans;
-    while (n > 1) {
-        if (((n + 1) / 2) & 1) {
+    vector<int> ans = {2};
+    while (cur != n) {
+        ll l = cur * 2 - 1, r = cur * 2 - 1;
+        bool ok = false;
+        for (int i = 0; i < 40; ++i) {
+            if (l <= n && n <= r) {
+                ok = true;
+                break;
+            }
+            l = l * 2 - 1;
+            r = r * 2 + 1;
+        }
+        if (ok) {
             ans.push_back(1);
-            n = (n + 1) / 2;
+            cur = cur * 2 - 1;
         }
         else {
             ans.push_back(2);
-            n /= 2;
+            cur = cur * 2 + 1;
         }
     }
     cout << ans.size() << "\n";
-    reverse(ans.begin(), ans.end());
     for (auto i : ans)
         cout << i << " ";
     cout << "\n";
