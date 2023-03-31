2f8f5cbc6550a33fe9715362f9e685cdeb6e6f7b
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-03-31 16:37:00 +0200

    Add solution for problem `A - Beautiful Sequence` Link: https://codeforces.com/contest/1810/problem/A Tags: brute force, greedy, *800 Ref: https://codeforces.com/contest/1810/submission/199958816

--- codeforces/1810/A.cpp
+++ codeforces/1810/A.cpp
@@ -1,13 +1,28 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(1000,10000)(rng)
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 void solve() {
-    int n, k;
+    int n;
     cin >> n;
     bool ok = false;
-    for (int i = 1; i <= n; ++i) {
-        cin >> k;
-        ok |= k <= i;
+    int a[n];
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+        ok |= (a[i] <= i + 1);
     }
     if (ok)
         cout << "YES\n";
