6dbe579a462ba317cd399331599424d010ac7fbd
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-04-02 16:37:00 +0200

    Add solution for problem `A - We Need the Zero` Link: https://codeforces.com/contest/1805/problem/A Tags: bitmasks, brute force, *800 Ref: https://codeforces.com/contest/1805/submission/200388561

--- codeforces/1805/A.cpp
+++ codeforces/1805/A.cpp
@@ -1,5 +1,19 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(1000,10000)(rng)
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 void solve() {
     int n, x = 0, k;
@@ -8,12 +22,14 @@ void solve() {
         cin >> k;
         x ^= k;
     }
-    if (n & 1)
-        cout << x << "\n";
-    else if (x == 0)
-        cout << "0\n";
+    if (n % 2 == 0) {
+        if (x == 0)
+            cout << "0\n";
+        else
+            cout << "-1\n";
+    }
     else
-        cout << "-1\n";
+        cout << x << "\n";
 }
 
 int main() {
