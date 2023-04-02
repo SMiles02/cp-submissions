4635621ace5632c9fc21ec24baa02f858c655278
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-04-02 16:40:00 +0200

    Add solution for problem `B - The String Has a Target` Link: https://codeforces.com/contest/1805/problem/B Tags: greedy, strings, *800 Ref: https://codeforces.com/contest/1805/submission/200391595

--- codeforces/1805/B.cpp
+++ codeforces/1805/B.cpp
@@ -1,18 +1,35 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(1000,10000)(rng)
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 void solve() {
-    int n, j = 0;
+    int n, id;
     cin >> n;
     string s;
     cin >> s;
-    for (int i = 1; i < n; ++i)
-        if (s[i] <= s[j])
-            j = i;
-    cout << s[j];
-    for (int i = 0; i < j; ++i)
+    char m = 'z';
+    for (int i = 0; i < n; ++i)
+        if (s[i] <= m) {
+            m = s[i];
+            id = i;
+        }
+    cout << s[id];
+    for (int i = 0; i < id; ++i)
         cout << s[i];
-    for (int i = j + 1; i < n; ++i)
+    for (int i = id + 1; i < n; ++i)
         cout << s[i];
     cout << "\n";
 }
