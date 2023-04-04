1041faacc4d797ee402bc801652b4ed1c1644ffc
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-04-04 16:42:00 +0200

    Add solution for problem `B - Conveyor Belts` Link: https://codeforces.com/contest/1811/problem/B Tags: implementation, math, *1000 Ref: https://codeforces.com/contest/1811/submission/200698656

--- codeforces/1811/B.cpp
+++ codeforces/1811/B.cpp
@@ -1,10 +1,26 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(1000,10000)(rng)
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 void solve() {
-    int n, a, b, c, d;
+    int n, a, b, c, d, x, y;
     cin >> n >> a >> b >> c >> d;
-    cout << abs(min({a, b, n - a + 1, n - b + 1}) - min({c, d, n - c + 1, n - d + 1})) << "\n";
+    x = min({a, b, n - a + 1, n - b + 1});
+    y = min({c, d, n - c + 1, n - d + 1});
+    cout << abs(x - y) << "\n";
 }
 
 int main() {
