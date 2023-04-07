c99a6850276f97d3da39a8a1466fb0fc3bde8c43
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-04-07 19:10:00 +0200

    Add solution for problem `D - Mishka and Interesting sum` Link: https://codeforces.com/contest/703/problem/D Tags: data structures, *2100 Ref: https://codeforces.com/contest/703/submission/201169193

--- codeforces/703/D.cpp
+++ codeforces/703/D.cpp
@@ -1,13 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+const int N = 1e6 + 1;
+
 struct BIT {
-    int n;
+    int n, rtn;
     vector<int> bit;
-    BIT(int n) : n(n), bit(n + 1) {}
+    BIT(int n) : n(n), bit(n + 1,0) {}
     void update(int x, int d) { for (int i = x; i <= n; i += i & -i) bit[i] ^= d; }
     int query(int x) {
-        int rtn = 0;
+        rtn = 0;
         for (int i = x; i; i -= i & -i) rtn ^= bit[i];
         return rtn;
     }
@@ -16,18 +18,20 @@ struct BIT {
 
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
-    int n, q;
+    int n, q, l, r, id;
     cin >> n;
-    vector<int> a(n + 1);
+    int a[n + 1], p[n + 1];
+    p[0] = 0;
     map<int, int> last;
     BIT bit(n);
-    for (int i = 1; i <= n; ++i)
+    for (int i = 1; i <= n; ++i) {
         cin >> a[i];
+        p[i] = p[i - 1] ^ a[i];
+    }
     cin >> q;
-    vector<int> ans(q);
+    int ans[q];
     vector<vector<array<int, 3>>> queries(n + 1);
     for (int i = 0; i < q; ++i) {
-        int l, r;
         cin >> l >> r;
         queries[r].push_back({l, i});
     }
