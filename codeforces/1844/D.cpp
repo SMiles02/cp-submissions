7478a7fcef327936f8391f02683c1bc02f21dfa2
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-07-11 17:17:00 +0200

    Add solution for problem `D - Row Major` Link: https://codeforces.com/contest/1844/problem/D Tags: constructive algorithms, greedy, math, number theory, strings, *1400 Ref: https://codeforces.com/contest/1844/submission/213340693

--- codeforces/1844/D.cpp
+++ codeforces/1844/D.cpp
@@ -1,18 +1,34 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(1000,10000)(rng)
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
+
+string alpha = "abcdefghijklmnopqrstuvwxyz";
 
 void solve() {
     int n;
     cin >> n;
-    vector<int> v;
-    string ans = "";
-    for (int i = 1; i * i <= n; ++i) {
+    vector<int> v = {1};
+    for (int i = 2; i * i <= n; ++i) {
         if (n % i == 0) {
             v.push_back(i);
             v.push_back(n / i);
         }
     }
-    for (int i = 0; i < n; ++i) {
+    string ans = "a";
+    for (int i = 1; i < n; ++i) {
         vector<bool> done(26);
         for (int j : v) {
             if (i >= j) {
@@ -21,7 +37,7 @@ void solve() {
         }
         for (int j = 0; j < 26; ++j) {
             if (!done[j]) {
-                ans += (char)('a' + j);
+                ans += alpha[j];
                 break;
             }
         }
