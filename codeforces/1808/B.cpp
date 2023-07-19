7e85a638ba3c98cfdb751908c275cdea7b840618
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-07-20 00:37:00 +0200

    Add solution for problem `B - Playing in a Casino` Link: https://codeforces.com/contest/1808/problem/B Tags: math, sortings, *1200 Ref: https://codeforces.com/contest/1808/submission/214543903

--- codeforces/1808/B.cpp
+++ codeforces/1808/B.cpp
@@ -1,5 +1,19 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(1000,10000)(rng)
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 void solve() {
     int n, m;
@@ -12,7 +26,7 @@ void solve() {
             v[j].push_back(x);
         }
     }
-    long long ans = 0;
+    ll ans = 0;
     for (int i = 0; i < m; ++i) {
         sort(v[i].begin(), v[i].end());
         for (int j = 0; j < n; ++j) {
