7aa621621809bd6628672462dfcc88c34b24f84c
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-07-21 01:02:00 +0200

    Add solution for problem `E - Tree Reconstruction` Link: https://codeforces.com/contest/1041/problem/E Tags: constructive algorithms, data structures, graphs, greedy, *1900 Ref: https://codeforces.com/contest/1041/submission/214676158

--- codeforces/1041/E.cpp
+++ codeforces/1041/E.cpp
@@ -1,5 +1,19 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(1000,10000)(rng)
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 int main() {
     ios_base::sync_with_stdio(0); cin.tie(0);
@@ -33,6 +47,7 @@ int main() {
             }
             cur.push_back(i);
             for (int j = 1; j < cur.size(); ++j) {
+                // cout << cur[j - 1] << " " << cur[j] << "\n";
                 ans.push_back({cur[j - 1], cur[j]});
             }
         }
