6358be38f93b8d64de39c840fa4bf56a808968fb
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-07-21 19:08:00 +0200

    Add solution for problem `G - The Morning Star` Link: https://codeforces.com/contest/1850/problem/G Tags: combinatorics, data structures, geometry, implementation, math, sortings, *1500 Ref: https://codeforces.com/contest/1850/submission/214938173

--- codeforces/1850/G.cpp
+++ codeforces/1850/G.cpp
@@ -1,10 +1,24 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(1000,10000)(rng)
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 void solve() {
     int n;
     cin >> n;
-    long long ans = 0;
+    ll ans = 0;
     vector<map<int, int>> m(4);
     for (int i = 0; i < n; ++i) {
         int x, y;
