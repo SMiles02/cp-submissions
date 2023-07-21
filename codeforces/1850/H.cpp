35241e74ddd4e2099d8882ffe44775e5d7a00a58
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-07-21 18:16:00 +0200

    Add solution for problem `H - The Third Letter` Link: https://codeforces.com/contest/1850/problem/H Tags: dfs and similar, dsu, graphs, greedy, implementation, *1700 Ref: https://codeforces.com/contest/1850/submission/214912367

--- codeforces/1850/H.cpp
+++ codeforces/1850/H.cpp
@@ -1,10 +1,24 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(1000,10000)(rng)
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 const int N = 2e5 + 1;
-const long long INF = 1e18;
+const ll INF = 1e18;
 vector<array<int, 2>> e[N];
-long long val[N];
+ll val[N];
 bool ok;
 
 void dfs(int c) {
