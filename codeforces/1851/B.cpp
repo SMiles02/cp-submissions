a05145cebaf931e2fc28adf9ebaa0abf4732a2b7
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-07-25 16:47:00 +0200

    Add solution for problem `B - Parity Sort` Link: https://codeforces.com/contest/1851/problem/B Tags: greedy, sortings, two pointers, *800 Ref: https://codeforces.com/contest/1851/submission/215518166

--- codeforces/1851/B.cpp
+++ codeforces/1851/B.cpp
@@ -1,22 +1,42 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(1000,10000)(rng)
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 void solve() {
     int n;
     cin >> n;
+    vector<vector<int>> v(2, vector<int>());
     vector<int> a(n), b(n);
     for (int i = 0; i < n; ++i) {
         cin >> a[i];
-        b[i] = a[i];
+        v[a[i] & 1].push_back(a[i]);
+    }
+    sort(v[0].rbegin(), v[0].rend());
+    sort(v[1].rbegin(), v[1].rend());
+    for (int i = 0; i < n; ++i) {
+        b[i] = v[a[i] & 1].back();
+        v[a[i] & 1].pop_back();
     }
     sort(a.begin(), a.end());
-    for (int i = 0; i < n; ++i) {
-        if ((a[i] - b[i]) % 2) {
-            cout << "NO\n";
-            return;
-        }
+    if (a == b) {
+        cout << "YES\n";
     }
-    cout << "YES\n";
+    else {
+        cout << "NO\n";
+    }
 }
 
 int main() {
