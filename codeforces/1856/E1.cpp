156d257eb69ba61828af74feeb07c3e091489f0c
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-08-05 19:30:00 +0200

    Add solution for problem `E1 - PermuTree (easy version)` Link: https://codeforces.com/contest/1856/problem/E1 Tags: dfs and similar, dp, trees, *1800 Ref: https://codeforces.com/contest/1856/submission/217357605

---
+++ codeforces/1856/E1.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+const int N = 5005;
+int sub[N];
+bitset<N> dp[N];
+ll ans;
+vector<int> e[N];
+
+void dfs(int c) {
+    dp[c][0] = 1;
+    for (int i : e[c]) {
+        dfs(i);
+        dp[c] |= dp[c] << sub[i];
+        sub[c] += sub[i];
+    }
+    ll cur = 0;
+    for (int i = 1; i < sub[c]; ++i) {
+        if (dp[c][i]) {
+            cur = max(cur, 1LL * i * (sub[c] - i));
+        }
+    }
+    ans += cur;
+    ++sub[c];
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int n;
+    cin >> n;
+    for (int i = 2; i <= n; ++i) {
+        int p;
+        cin >> p;
+        e[p].push_back(i);
+    }
+    dfs(1);
+    cout << ans;
+    return 0;
+}
