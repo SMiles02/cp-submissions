91d85f00bdd1d19564b7b5f69becd96122e0dd91
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-08-26 16:52:00 +0200

    Add solution for problem `C - Divisor Chain` Link: https://codeforces.com/contest/1864/problem/C Tags: bitmasks, constructive algorithms, math, number theory, *1300 Ref: https://codeforces.com/contest/1864/submission/220535893

--- codeforces/1864/C.cpp
+++ codeforces/1864/C.cpp
@@ -1,15 +1,31 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(1000,10000)(rng)
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 void solve() {
     int n;
     cin >> n;
     vector<int> ans = {n};
     while (__builtin_popcount(n) > 1) {
-        ans.push_back(n -= n & -n);
+        n -= n & -n;
+        ans.push_back(n);
     }
     while (n > 1) {
-        ans.push_back(n /= 2);
+        ans.push_back(n / 2);
+        n /= 2;
     }
     cout << ans.size() << "\n";
     for (int i : ans) {
