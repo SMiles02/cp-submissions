d44a4e31a03163803b697447e0c4898b7d86c0ae
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-08-26 16:40:00 +0200

    Add solution for problem `A - Increasing and Decreasing` Link: https://codeforces.com/contest/1864/problem/A Tags: constructive algorithms, greedy, implementation, math, *800 Ref: https://codeforces.com/contest/1864/submission/220524633

--- codeforces/1864/A.cpp
+++ codeforces/1864/A.cpp
@@ -1,5 +1,19 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(1000,10000)(rng)
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 void solve() {
     int n, x, y;
@@ -9,8 +23,8 @@ void solve() {
     }
     else {
         vector<int> v(n);
-        v[0] = x;
         v[n - 1] = y;
+        v[0] = x;
         for (int i = 1; i + 1 < n; ++i) {
             v[n - i - 1] = v[n - i] - i;
         }
