07c10b0ac05410ae5384099bf7dc23b5d7980433
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-08-31 17:35:00 +0200

    Add solution for problem `C - Queries for the Array` Link: https://codeforces.com/contest/1861/problem/C Tags: data structures, dfs and similar, implementation, strings, trees, *1600 Ref: https://codeforces.com/contest/1861/submission/221321314

--- codeforces/1861/C.cpp
+++ codeforces/1861/C.cpp
@@ -1,15 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int N = 2e5 + 1;
+const int N = 2e5 + 10;
 bitset<2> dp[N][2], source[N][2][2][2];
+
+void print(int i, int x, int y) {
+    cerr << i << " " << x << " " << y << ": " << dp[i][x][y] << "\n";
+}
 
 void solve() {
     int n, cur_len = 0;
     string s;
     cin >> s;
     n = s.size();
-    for (int i = 0; i <= n; ++i) {
+    for (int i = 0; i < n + 5; ++i) {
         for (int j = 0; j < 2; ++j) {
             for (int k = 0; k < 2; ++k) {
                 dp[i][j][k] = 0;
@@ -30,13 +34,23 @@ void solve() {
                         continue;
                     }
                     for (int x = 0; x < 2; ++x) {
-                        int y = 1 - (i > x || j == 0);
+                        int y = 1;
+                        if (i > x || j == 0) {
+                            y = 0;
+                        }
+                        // cout << i << " " << j << " " << x << " " << y << "\n";
                         dp[cur_len + 1][x][y] = 1;
                         source[cur_len + 1][x][y][i][j] = 1;
                     }
                 }
             }
             ++cur_len;
+            // for (int i = 0; i < 2; ++i) {
+            //     for (int j = 0; j < 2; ++j) {
+            //         cout << i << " " << j << ": " << dp[cur_len][i][j] << "\n";
+            //     }
+            // }
+            // cout << "\n";
         }
         else if (s[c] == '-') {
             for (int i = 0; i < 2; ++i) {
@@ -48,21 +62,36 @@ void solve() {
                 for (int y = 0; y < 2; ++y) {
                     for (int i = 0; i < 2; ++i) {
                         for (int j = 0; j < 2; ++j) {
+                            // if (x == 0 && y == 0 && i == 1 && j == 1) {
+                            //     cerr << "test\n";
+                            //     cerr << dp[cur_len][x][y] << " " << source[cur_len][x][y][i][j] << "\n";
+                            // }
                             if (dp[cur_len][x][y] == 1 && source[cur_len][x][y][i][j] == 1) {
                                 dp[cur_len - 1][i][j] = 1;
+                                // print(cur_len - 1, i, j);
                             }
                             source[cur_len][x][y][i][j] = 0;
                         }
                     }
                     dp[cur_len][x][y] = 0;
                 }
+                // print(cur_len - 1, 1, 1);
             }
             --cur_len;
+            // print(cur_len, 1, 1);
+            // for (int i = 0; i < 2; ++i) {
+            //     for (int j = 0; j < 2; ++j) {
+            //         cout << i << " " << j << ": " << dp[cur_len][i][j] << "\n";
+            //     }
+            // }
+            // cout << "\n";
         }
         else {
-            bool found = false;
             for (int x = 0; x < 2; ++x) {
                 dp[cur_len][x][1 - (s[c] - '0')] = 0;
+            }
+            bool found = false;
+            for (int x = 0; x < 2; ++x) {
                 if (dp[cur_len][x][(s[c] - '0')]) {
                     found = true;
                 }
@@ -71,7 +100,16 @@ void solve() {
                 cout << "NO\n";
                 return;
             }
+            // for (int i = 0; i < 2; ++i) {
+            //     for (int j = 0; j < 2; ++j) {
+            //         cout << i << " " << j << ": " << dp[cur_len][i][j] << "\n";
+            //     }
+            // }
+            // cout << "\n";
         }
+        // if (c == 1) {
+        //     cerr << source[cur_len][0][0][1][1] << "\n";
+        // }
     }
     cout << "YES\n";
 }
