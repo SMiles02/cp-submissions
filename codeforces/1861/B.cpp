b8ca4c99854d058adeef929af9aff99af1f34114
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-08-31 16:43:00 +0200

    Add solution for problem `B - Two Binary Strings` Link: https://codeforces.com/contest/1861/problem/B Tags: constructive algorithms, dp, greedy, *1000 Ref: https://codeforces.com/contest/1861/submission/221284788

--- codeforces/1861/B.cpp
+++ codeforces/1861/B.cpp
@@ -1,5 +1,19 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(1000,10000)(rng)
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 void solve() {
     string s, t;
