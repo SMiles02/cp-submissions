c1e211fdebe4d39d3d533e1f0b891f6f712d78d6
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2023-09-10 16:55:00 +0200

    Add solution for problem `B1 - Candy Party (Easy Version)` Link: https://codeforces.com/contest/1868/problem/B1 Tags: bitmasks, constructive algorithms, graphs, greedy, implementation, math, *1700 Ref: https://codeforces.com/contest/1868/submission/222756950

--- codeforces/1868/B1.cpp
+++ codeforces/1868/B1.cpp
@@ -1,11 +1,25 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(1000,10000)(rng)
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 void solve() {
     int n, avg;
     cin >> n;
-    long long sum = 0;
-    vector<int> a(n), p(32);
+    ll sum = 0;
+    vector<int> a(n), p(40), q(40);
     for (int i = 0; i < n; ++i) {
         cin >> a[i];
         sum += a[i];
@@ -20,12 +34,12 @@ void solve() {
             continue;
         }
         bool done = false;
-        for (int j = 0; j < 32; ++j) {
-            for (int k = 0; k < 32; ++k) {
+        for (int j = 0; j < 40; ++j) {
+            for (int k = 0; k < 40; ++k) {
                 if ((1LL << j) - (1LL << k) + a[i] == avg) {
                     done = true;
                     ++p[j];
-                    --p[k];
+                    ++q[k];
                 }
             }
         }
@@ -34,8 +48,8 @@ void solve() {
             return;
         }
     }
-    for (int i = 0; i < 32; ++i) {
-        if (p[i] != 0) {
+    for (int i = 0; i < 40; ++i) {
+        if (p[i] != q[i]) {
             cout << "NO\n";
             return;
         }
