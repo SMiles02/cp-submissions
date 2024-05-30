622979b9d3ffb640ef94e76f22209f5355873b89
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2024-05-30 19:02:00 +0200

    Add solution for problem `C - Job Interview` Link: https://codeforces.com/contest/1976/problem/C Tags: binary search, dp, greedy, implementation, two pointers, *1600 Ref: https://codeforces.com/contest/1976/submission/263365338

--- codeforces/1976/C..cpp
+++ codeforces/1976/C..cpp
@@ -1,12 +1,26 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(1000,10000)(rng)
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 void solve() {
     int n, m, x = 0, cnt = 0, last = 0;
     cin >> n >> m;
-    long long tot = 0;
+    ll tot = 0;
     vector<int> a(n + m + 1), b(n + m + 1);
-    vector<long long> ans(n + m + 1);
+    vector<ll> ans(n + m + 1);
     for (int i = 0; i < n + m + 1; ++i) {
         cin >> a[i];
     }
