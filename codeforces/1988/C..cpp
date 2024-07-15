862cb70fe215c661164e6a1e7b6304fcf7cd99c0
Suneet Mahajan <mahajan.suneet2002@gmail.com>
2024-07-15 16:02:00 +0100

    Add solution for problem `C - Increasing Sequence with Fixed OR` Link: https://codeforces.com/contest/1988/problem/C Tags: bitmasks, constructive algorithms, greedy Ref: https://codeforces.com/contest/1988/submission/270673250

--- codeforces/1988/C..cpp
+++ codeforces/1988/C..cpp
@@ -1,8 +1,22 @@
 #include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
 using namespace std;
+// mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+// uniform_int_distribution<int>(1000,10000)(rng)
+
+string to_upper(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+    return a;
+}
+
+string to_lower(string a) {
+    for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+    return a;
+}
 
 void solve() {
-    long long n;
+    ll n;
     cin >> n;
     int k = __builtin_popcountll(n);
     if (k == 1) {
@@ -11,7 +25,7 @@ void solve() {
     }
     cout << k + 1 << "\n";
     for (int i = 0; i <= k; ++i) {
-        long long ans = 0;
+        ll ans = 0;
         int cnt = 0;
         for (int j = 59; j >= 0; --j) {
             if (n & (1LL << j)) {
